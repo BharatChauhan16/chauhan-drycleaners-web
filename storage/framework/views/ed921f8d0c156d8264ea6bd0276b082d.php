6d8bef532a87f53df88cfca406ff8486ad52614d
BharatChauhan16 <rajputbharat1623@gmail.com>
2026-04-07 11:51:02 +0530

    initial commit

---
+++ storage/framework/views/ed921f8d0c156d8264ea6bd0276b082d.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Chauhan DryCleaners</title>
+    <?php echo app('Illuminate\Foundation\Vite')(['resources/css/app.css', 'resources/js/app.js']); ?>
+</head>
+<body>
+    <div id="app"></div>
+</body>
+</html><?php /**PATH C:\xampp\htdocs\chauhan-drycleaners\resources\views/app.blade.php ENDPATH**/ ?>
